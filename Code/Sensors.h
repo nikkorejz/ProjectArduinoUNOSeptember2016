da3be1059a4461ea650db65d630b9c6d2c369582
Nikita G <nikita_gromik@mail.ru>
2016-09-09 23:00:39 +0800

    Update Sensors.h

--- Code/Sensors.h
+++ Code/Sensors.h
@@ -1,13 +1,13 @@
-#include <Arduino.h>
+#include <Arduino.h> //Просто так надо
 
 class Sensors {
   public:
-    Sensors();
-    int getAction();
-    boolean getRight();
-    boolean getLeft();
-    void attach(int pinLeft, int pinRight);
+    Sensors(); //Пустой конструктор
+    int getAction(); //Функция для возвращения числа, по которому определяется дальнейшее движение (Использовалось в старой версии, сейчас не нужно)
+    boolean getRight(); //Значения правого датчика
+    boolean getLeft(); //Левого
+    void attach(int pinLeft, int pinRight); //Подключение сенсоров
   private:
-    boolean leftSensor, rightSensor;
+    boolean leftSensor, rightSensor; 
 };
 
