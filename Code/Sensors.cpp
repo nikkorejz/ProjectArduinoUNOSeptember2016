d2ea4860baf597d69d5c3dbd4b5e45e4515bf521
Nikita G <nikita_gromik@mail.ru>
2016-09-09 23:03:10 +0800

    Update Sensors.cpp

--- Code/Sensors.cpp
+++ Code/Sensors.cpp
@@ -1,4 +1,4 @@
-#include "Sensors.h"
+#include "Sensors.h" 
 
 #define STATE_STOP 0
 #define STATE_FORWARD 1
@@ -11,28 +11,28 @@ Sensors::Sensors() {
 }
 
 void Sensors::attach(int pinLeft, int pinRight) {
-  this -> leftSensor = !digitalRead(pinLeft);
-  this -> rightSensor = !digitalRead(pinRight);
+  this -> leftSensor = !digitalRead(pinLeft); //THIS -> НЕ обязателен, если имя переменной и параметра метода НЕ совпадают
+  this -> rightSensor = !digitalRead(pinRight); //Это тип BOOLEAN потому что датчик нам дает всего 2 значения, если хотите - используйте INTEGER, никто не запрещает
 }
 
 int Sensors::getAction() {
   
-  if (this -> leftSensor && this -> rightSensor) {
+  if (this -> leftSensor && this -> rightSensor) { //Если значения одинаковы, то вернём единицу
     return STATE_FORWARD;
   } 
-  else if (!this -> leftSensor && this -> rightSensor) {
+  else if (!this -> leftSensor && this -> rightSensor) { //Если левый врёт, то возвращаем тройку
     return STATE_RIGHT; 
   } 
-  else if (!this -> leftSensor && !this -> rightSensor) {
+  else if (!this -> leftSensor && !this -> rightSensor) { //Если оба врут, то возвращаем четверку
     return STATE_BACKWARD;  
   }
-  else {
+  else { //Иначе двойку
     return STATE_LEFT;
   }
   
 }
   
-boolean Sensors::getRight() {
+boolean Sensors::getRight() { //Вернем PRIVATE переменные
   return this -> rightSensor; 
 }
 
